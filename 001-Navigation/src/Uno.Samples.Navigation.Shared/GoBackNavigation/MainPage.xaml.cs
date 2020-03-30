06113d7179c1e418af2479f79a22016ee4ad9089
Andrew Hoefling <andrew@hoeflingsoftware.com>
2020-03-29 22:02:46 -0400

    Added goback navigation sample

---
+++ 001-Navigation/src/Uno.Samples.Navigation.Shared/GoBackNavigation/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Uno.Samples.Navigation.GoBackNavigation
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (Frame.CanGoBack)
+                Frame.GoBack();
+        }
+    }
+}
