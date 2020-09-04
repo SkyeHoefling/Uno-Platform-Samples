fa75b3aec2d7304bb94e472a12a8038f73a285d5
Andrew Hoefling <andrew@hoeflingsoftware.com>
2020-09-03 22:04:15 -0400

    Completed command bar tabbed sample

--- CommandBar/CommandBarSample/CommandBarSample.Shared/App.xaml.cs
+++ CommandBar/CommandBarSample/CommandBarSample.Shared/App.xaml.cs
@@ -1,20 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using System;
+using Microsoft.Extensions.Logging;
 using Windows.ApplicationModel;
 using Windows.ApplicationModel.Activation;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using CommandBarSample.Shared.Views;
 
 namespace CommandBarSample
 {
--- CommandBar/CommandBarSample/CommandBarSample.Shared/Views/MainPage.xaml.cs
+++ CommandBar/CommandBarSample/CommandBarSample.Shared/Views/MainPage.xaml.cs
@@ -1,30 +1,44 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
-namespace CommandBarSample
+namespace CommandBarSample.Shared.Views
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
+        Type currentPage;
         public MainPage()
         {
-            this.InitializeComponent();
+            InitializeComponent();
+            currentPage = typeof(HomePage);
+            contentView.Navigate(currentPage);
+        }
+
+        void OnAppBarButtonTapped(object sender, RoutedEventArgs args)
+        {
+            if (sender is FrameworkElement element)
+            {
+                var tag = element.Tag.ToString();
+                Type page = null;
+                switch (tag)
+                {
+                    case "Home":
+                        page = typeof(HomePage);
+                        break;
+                    case "Info":
+                        page = typeof(InfoPage);
+                        break;
+                    case "About":
+                        page = typeof(AboutPage);
+                        break;
+                }
+
+                if (currentPage != page)
+                {
+                    currentPage = page;
+                    contentView.Navigate(page);
+                }
+            }
         }
     }
 }
